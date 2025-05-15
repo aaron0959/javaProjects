06e358503bfcb0ec6e9c21ad7e6c2e31a4d5151b
Aaron <98107414+aaron0959@users.noreply.github.com>
2025-05-15 12:17:32 +0100

    Delete API_Code_CleanSMR directory

--- API_Code_CleanSMR/app/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-from app.routes.observation_routes import observation_routes
-
-observation_blueprint = Blueprint('observations', __name__)
-observation_blueprint.register_blueprint(observation_routes)
